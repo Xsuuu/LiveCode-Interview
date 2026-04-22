33ee0ad1e0bda8082e8c83097afc4919f4c53d2d
xsuuu <434893886@qq.com>
2026-04-22 10:25:20 +0800

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,29 @@
 //const express = require("express")
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
+
+// console.log(ENV.PORT);
+// console.log(ENV.DB_URL);
 
 const app = express();
+const __dirname = path.resolve();
+//用了 "type": "module"，切换到 ES Module 之后，__dirname 就消失了，直接用会报错。
+// 所以用这行代码手动模拟
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'success from backend' });
+app.get('/health', (req, res) => {
+  res.status(200).json({ msg: 'api is up and running' });
 });
+app.get('/books', (req, res) => {
+  res.status(200).json({ msg: 'this is the bools endpoint' });
+});
+
+//make our app ready for deployment
+if(ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
+
 app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT));
